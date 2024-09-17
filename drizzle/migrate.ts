07373e9671412e898f7255bd63cabd00705ee0d3
“H <vignesh.h@codecraft.co.in>
2024-09-17 21:39:36 +0530

    Installed eslint in the project

--- drizzle/migrate.ts
+++ drizzle/migrate.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
 import { migrate } from "drizzle-orm/mysql2/migrator";
 import mysql from "mysql2/promise";
-import { AppEnvs } from "../../read-env";
+import { AppEnvs } from "@/read-env";
 // import { books, members, transactions } from "./schema";
 
 //Database URL
@@ -17,7 +17,7 @@ const migrationClient = mysql.createPool({
 async function migrateDB() {
   await migrate(drizzle(migrationClient), {
     migrationsFolder:
-      "/home/shashankb/express-framework/nextjs-library-app/src/drizzle/migration", // Adjust this path to your migrations folder src/drizzle/migration
+      "/home/vigneshtr/projects/library-management-system-nextjs/drizzle/migration", // Adjust this path to your migrations folder src/drizzle/migration
   });
   await migrationClient.end();
 }
